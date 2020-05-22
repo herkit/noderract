a1f527d01b4317b7b46d707e35fc278385e73209
Henrik Grotle <henrik.grotle@gmail.com>
2020-05-22 12:59:34 +0200

    blob trigger

--- TesseractFunction/Ocr/index.js
+++ TesseractFunction/Ocr/index.js
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process');
+const tesseract = require('../lib/tesseract');
 
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
@@ -11,49 +11,8 @@ module.exports = async function (context, req) {
   if (m) {
     var imagedata = Buffer.from(m[2], 'base64');
 
-    console.log("Imagedata:", imagedata);
-
-    var tesseract = spawn('tesseract', ['stdin', 'stdout']);
-
-    console.log("Tesseract PID", tesseract.pid);
-
-    console.log("Stdin writable:", tesseract.stdin.writable);
-
-    tesseract.stdin.write(imagedata);
-    tesseract.stdin.end();
-
-    console.log("Stdout readable: ", tesseract.stdout.readable);
-
-    var end = new Promise(function (resolve, reject) {
-      var result = "";
-      tesseract.stdout.on('data', (chunk) => {
-        result += chunk.toString('utf8');
-        console.log(`Received ${chunk.length} bytes of data.`, chunk.toString('utf8'));
-      });
-
-      tesseract.stdout.on('end', () => {
-        console.log("Stdout readable at end: ", tesseract.stdout.readable);
-        resolve(result);
-      });
-
-      tesseract.on('close', (code) => {
-        var err = tesseract.stderr.read();
-        console.log(`child process close all stdio with code ${code}, ${err}`);
-        if (code != 0)
-          reject(err);
-      });
-
-      tesseract.on('exit', (code) => {
-        var err = tesseract.stderr.read();
-        console.log(`child process exited with code ${code}, ${err}`);
-        if (code != 0)
-          reject(err);
-      });
-
-    });
-
     try {
-      let result = await end;
+      let result = await tesseract(imagedata);
       console.log("Result:", result);
       context.res = {
         body: {
--- TesseractFunction/lib/tesseract.js
+++ TesseractFunction/lib/tesseract.js
@@ -1,4 +1,39 @@
+const { spawn } = require('child_process');
+
 module.exports = function(imagedata, options) 
 {
+  var tesseract = spawn('tesseract', ['stdin', 'stdout']);
 
+  tesseract.stdin.write(imagedata);
+  tesseract.stdin.end();
+
+  console.log("Stdout readable: ", tesseract.stdout.readable);
+
+  return new Promise(function (resolve, reject) {
+    var result = "";
+    tesseract.stdout.on('data', (chunk) => {
+      result += chunk.toString('utf8');
+      console.log(`Received ${chunk.length} bytes of data.`, chunk.toString('utf8'));
+    });
+
+    tesseract.stdout.on('end', () => {
+      console.log("Stdout readable at end: ", tesseract.stdout.readable);
+      resolve(result);
+    });
+
+    tesseract.on('close', (code) => {
+      var err = tesseract.stderr.read();
+      console.log(`child process close all stdio with code ${code}, ${err}`);
+      if (code != 0)
+        reject(err);
+    });
+
+    tesseract.on('exit', (code) => {
+      var err = tesseract.stderr.read();
+      console.log(`child process exited with code ${code}, ${err}`);
+      if (code != 0)
+        reject(err);
+    });
+
+  });
 }
